7d12e3ded83b1c81c067609c6650913a9dd2ec49
yamashita yuichi <crgone910428@gmail.com>
2025-01-14 15:20:57 +0900

    Dockerfileを修正

--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@
 # サブモジュールの初期化と更新
 git submodule update --init --recursive
 
+# 各サブモジュールをmainブランチに切り替え
+git submodule foreach 'git checkout main && git pull origin main'
+
 # Dockerコンテナのビルドと起動
 docker compose up -d
